5f3e8afcbe359a153622ff5620aa346e72fc040d
RXGabriel <gabriel2005rf@gmail.com>
2024-02-06 17:31:55 -0300

    Listing Users

--- public/main.js
+++ public/main.js
@@ -21,6 +21,20 @@ function renderUserList() {
   });
 }
 
+function addMessage(type, user, msg) {
+  let ul = document.querySelector(".chatList");
+
+  switch (type) {
+    case "status":
+      ul.innerHTML += '<li class="m-status">' + msg + "</li>";
+      break;
+    case "msg":
+      ul.innerHTML +=
+        '<li class="m-txt"><span>' + user + "</span>" + msg + "</li>";
+      break;
+  }
+}
+
 loginInput.addEventListener("keyup", (event) => {
   if (event.keyCode === 13) {
     let name = loginInput.value.trim();
@@ -39,6 +53,21 @@ socket.on("user-ok", (list) => {
   chatPage.style.display = "flex";
   textInput.focus();
 
+  addMessage("status", null, "Conectado");
+
   userList = list;
+  renderUserList();
+});
+
+socket.on("list-update", (data) => {
+  if (data.joined) {
+    addMessage("status", null, data.joined + " entrou no chat");
+  }
+
+  if (data.left) {
+    addMessage("status", null, data.left + " saiu do chat");
+  }
+
+  userList = data.list;
   renderUserList();
 });
