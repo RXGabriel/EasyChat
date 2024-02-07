32fca3ede2df677ddfee27a8580e2cb66817db82
RXGabriel <gabriel2005rf@gmail.com>
2024-02-07 16:13:46 -0300

    Additional adjustments

--- public/main.js
+++ public/main.js
@@ -3,73 +3,44 @@ const socket = io();
 let userName = "";
 let userList = [];
 
-let loginPage = document.querySelector("#loginPage");
-let chatPage = document.querySelector("#chatPage");
-
-let loginInput = document.querySelector("#loginNameInput");
-let textInput = document.querySelector("#chatTextInput");
-
-loginPage.style.display = "flex";
-chatPage.style.display = "none";
+const loginPage = document.querySelector("#loginPage");
+const chatPage = document.querySelector("#chatPage");
+const loginInput = document.querySelector("#loginNameInput");
+const textInput = document.querySelector("#chatTextInput");
 
 function renderUserList() {
-  let ul = document.querySelector(".userList");
+  const ul = document.querySelector(".userList");
   ul.innerHTML = "";
-
-  userList.forEach((i) => {
-    ul.innerHTML += "<li>" + i + "</li>";
+  userList.forEach((name) => {
+    ul.innerHTML += "<li>" + name + "</li>";
   });
 }
 
 function addMessage(type, user, msg) {
-  let ul = document.querySelector(".chatList");
-
+  const ul = document.querySelector(".chatList");
   switch (type) {
     case "status":
       ul.innerHTML += '<li class="m-status">' + msg + "</li>";
       break;
     case "msg":
-      if (userName == user) {
-        ul.innerHTML +=
-          '<li class="m-txt"><span class="me">' +
-          user +
-          ": </span>" +
-          msg +
-          "</li>";
-      } else {
-        ul.innerHTML +=
-          '<li class="m-txt"><span>' + user + ": </span>" + msg + "</li>";
-      }
+      const senderName = user === userName ? "Você" : user;
+      const messageClass = user === userName ? "m-txt-me" : "m-txt";
+      ul.innerHTML += `<li class="${messageClass}"><span>${senderName}: </span>${msg}</li>`;
       break;
   }
   ul.scrollTop = ul.scrollHeight;
 }
 
-loginInput.addEventListener("keyup", (event) => {
-  if (event.keyCode === 13) {
-    let name = loginInput.value.trim();
-
-    if (name != "") {
-      userName = name;
-      document.title = "Chat(" + userName + ")";
-
-      socket.emit("join-request", userName);
-    }
-  }
-});
-
 textInput.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
-    let txt = textInput.value.trim();
+    const txt = textInput.value.trim();
     textInput.value = "";
-
-    if (txt != "") {
+    if (txt !== "") {
       if (!userName) {
         alert("Por favor, faça login primeiro!");
         return;
       }
 
-      addMessage("msg", userName, txt);
       socket.emit("send-msg", txt);
     }
   }
@@ -79,43 +50,50 @@ socket.on("user-ok", (list) => {
   loginPage.style.display = "none";
   chatPage.style.display = "flex";
   textInput.focus();
-
   addMessage("status", null, "Conectado!");
-
   userList = list;
   renderUserList();
 });
 
 socket.on("list-update", (data) => {
   if (data.joined) {
-    addMessage("status", null, data.joined.userName + " entrou no chat!");
+    addMessage("status", null, data.joined.userName + " entrou no chat.");
   }
-
   if (data.left) {
-    addMessage("status", null, data.left.userName + " saiu do chat!");
+    addMessage("status", null, data.left.userName + " saiu do chat.");
   }
-
   userList = data.list;
   renderUserList();
 });
 
 socket.on("show-msg", (data) => {
-  addMessage("msg", data.user, data.message);
+  addMessage("msg", data.userName, data.txt);
 });
 
 socket.on("disconnect", () => {
-  addMessage("status", null, "Você foi desconectado do servidor!");
+  addMessage("status", null, "Você foi desconectado!");
   userList = [];
   renderUserList();
 });
 
 socket.on("reconnect_error", () => {
-  addMessage("status", null, "Tentando reconectar...");
+  addMessage("status", null, "Falha ao se reconectar!");
 });
 
 socket.on("reconnect", () => {
   addMessage("status", null, "Reconectado!");
   if (userName !== "") {
-    socket.emit("join-request", userName);
+    socket.emit("join-request", { username: userName });
+  }
+});
+
+loginInput.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    const name = loginInput.value.trim();
+    if (name !== "") {
+      userName = name;
+      document.title = "Chat (" + userName + ")";
+      socket.emit("join-request", { username: userName });
+    }
   }
 });
--- server.js
+++ server.js
@@ -14,27 +14,34 @@ app.use(express.static(path.join(__dirname, "public")));
 let connectedUsers = [];
 
 io.on("connection", (socket) => {
-  console.log("Connection established");
+  console.log("Conexão com o servidor iniciada!");
 
-  socket.on("join", (userName) => {
-    socket.userName = userName;
-    connectedUsers.push(userName);
+  socket.on("join-request", (userData) => {
+    const { username } = userData;
+    socket.userName = username;
+    connectedUsers.push(username);
     console.log(connectedUsers);
-
     socket.emit("user-ok", connectedUsers);
     socket.broadcast.emit("list-update", {
-      joined: userName,
+      joined: { userName: username },
       list: connectedUsers,
     });
   });
 
   socket.on("disconnect", () => {
-    connectedUsers = connectedUsers.filter((u) => u != socket.userName);
+    connectedUsers = connectedUsers.filter((u) => u !== socket.userName);
     console.log(connectedUsers);
-
     socket.broadcast.emit("list-update", {
-      left: socket.userName,
+      left: { userName: socket.userName },
       list: connectedUsers,
     });
+  });
+
+  socket.on("send-msg", (txt) => {
+    const msgData = {
+      userName: socket.userName,
+      txt: txt,
+    };
+    io.emit("show-msg", msgData);
   });
 });
